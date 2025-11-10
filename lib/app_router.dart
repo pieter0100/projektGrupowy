230db11d3d9aac2795482048c62eeab5372c0a5f
Piotr Kierznowski <s197652@student.pg.edu.pl>
2025-11-10 15:57:43 +0100

    reforamt pliku z routingiem

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:projekt_grupowy/screens/home_screen.dart';
 import 'package:projekt_grupowy/screens/level_screen.dart';
 import 'package:projekt_grupowy/screens/profile_screen.dart';
-import 'main.dart';
 
 final GoRouter router = GoRouter(
   routes: <RouteBase>[
