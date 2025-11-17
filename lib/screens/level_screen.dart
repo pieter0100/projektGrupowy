3e12c64bf2ce1d1d5325c0c5b619c7e084e2c6ec
Weronika <s198226@student.pg.edu.pl>
2025-11-17 11:49:53 +0100

    dodanie ikon do paska gornego

--- lib/screens/level_screen.dart
+++ lib/screens/level_screen.dart
@@ -8,9 +8,46 @@ class LevelScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Level screen'),
-        backgroundColor: Color(0xFFE5E5E5),
-        scrolledUnderElevation: 0.0,
+        title: Row(
+    mainAxisSize: MainAxisSize.min,
+    children: [
+      const Text(
+        'Level screen',
+        style: TextStyle(
+          fontSize: 20,
+          color: Colors.black,
+        ),
+      ),
+      const SizedBox(width: 8),
+      Icon(
+        Icons.local_fire_department,
+        color: Color(0xFFEB9F4A),
+        size: 29,
+      ),
+      const SizedBox(width: 4),
+      const Text(
+        '3',
+        style: TextStyle(
+          fontSize: 23,
+          color: Color(0xFFEB9F4A),
+        ),
+      ),
+      const SizedBox(width: 8),
+      Icon(
+        Icons.diamond,
+        color: Color(0xFF338F9B),
+        size: 29,
+      ),
+      const SizedBox(width: 4),
+      const Text(
+        '1432 XP',
+        style: TextStyle(
+          fontSize: 23,
+          color: Color(0xFF338F9B),
+        ),
+      ),
+    ],
+  ),
       ),
       body: ListView.builder(
         itemCount: 10,
