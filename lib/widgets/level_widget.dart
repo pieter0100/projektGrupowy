01b8f48643752c4ada1b74560bdf4c5c903b6c20
Piotr Kierznowski <s197652@student.pg.edu.pl>
2025-11-24 15:25:59 +0100

    zmiana kontruktora przekazujemy parametr czy poziom jest lock czy nie

--- lib/widgets/level_widget.dart
+++ lib/widgets/level_widget.dart
@@ -2,8 +2,13 @@ import 'package:flutter/material.dart';
 
 class LevelWidget extends StatelessWidget {
   final String textInside;
+  final bool lock;
 
-  const LevelWidget(this.textInside, {Key? key}) : super(key: key);
+  const LevelWidget({
+    required this.textInside,
+    required this.lock,
+    super.key,                
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +16,7 @@ class LevelWidget extends StatelessWidget {
       margin: EdgeInsets.all(20),
       decoration: BoxDecoration(
         shape: BoxShape.circle,
-        border: Border.all(color: Color(0xFFC4C4C4), width: 10)
+        border: Border.all(color: Color(0xFFC4C4C4), width: 10),
       ),
       width: 140,
       height: 140,
@@ -22,32 +27,26 @@ class LevelWidget extends StatelessWidget {
             border: Border.all(color: Colors.white, width: 5),
             color: Color(0xFF72BFC7),
           ),
-          child: (textInside == 'lock') 
-          ? Center(
-            child: Opacity(
-              opacity: 0.5,
-              child: Icon(
-                Icons.lock,
-                color: Colors.black,
-                size: 50.0,
-              ),
-            )
-          )
-          :
-            Center(
-            child: Text(
-              textInside,
-              style: TextStyle(
-                fontFamily: 'Roboto',
-                // fontWeight: FontWeight.bold,
-                fontSize: 40.0,
-                color: Colors.white 
+          child: (textInside == 'lock')
+              ? Center(
+                  child: Opacity(
+                    opacity: 0.5,
+                    child: Icon(Icons.lock, color: Colors.black, size: 50.0),
+                  ),
+                )
+              : Center(
+                  child: Text(
+                    textInside,
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      // fontWeight: FontWeight.bold,
+                      fontSize: 40.0,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
-              ),
-            ),
         ),
       ),
     );
   }
 }
-
