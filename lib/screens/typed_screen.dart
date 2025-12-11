bc47dca13440817add227c148d5737fc4fa6a8b8
Piotr Kierznowski <s197652@student.pg.edu.pl>
2025-12-11 11:43:05 +0100

    dodanie wyswietlania sie podpowiedzi po wcisnieciu dont/' know

--- lib/screens/typed_screen.dart
+++ lib/screens/typed_screen.dart
@@ -2,8 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:projekt_grupowy/widgets/progress_bar_widget.dart';
 
-class TypedScreen extends StatelessWidget {
-  const TypedScreen({super.key});
+class TypedScreen extends StatefulWidget {
+  @override
+  _TypedScreenState createState() => _TypedScreenState();
+}
+
+class _TypedScreenState extends State<TypedScreen> {
+  String placeHolder = "Type the answer";
+
+  onSkip() {
+    setState(() {
+      // TODO pobierz odpowiedz do pytania i wstaw ja do placeholder'a
+      placeHolder = "podpowiedz";
+    });
+
+    // Uruchom licznik
+    Future.delayed(const Duration(seconds: 2), () {
+      // Sprawdzenie 'mounted' jest kluczowe!
+      // Zapobiega błędowi, jeśli użytkownik wyjdzie z ekranu
+      if (mounted) {
+        setState(() {
+          placeHolder = "Type the answer";
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,9 +34,7 @@ class TypedScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("(np Multiply x 2) dane z pobrane"),
         leading: IconButton(
-          icon: const Icon(
-            Icons.arrow_back_ios, 
-          ),
+          icon: const Icon(Icons.arrow_back_ios),
           onPressed: () => context.pop(), // akcja powrotu
         ),
         backgroundColor: Color(0xFFE5E5E5),
@@ -27,70 +48,78 @@ class TypedScreen extends StatelessWidget {
             ),
             Text(
               "(np 2 x 2) dane pobrane",
-              style: TextStyle(
-                fontSize: 48.0
-              ),
+              style: TextStyle(fontSize: 48.0),
               textAlign: TextAlign.center,
             ),
-            Padding(padding: EdgeInsetsGeometry.directional(start: 60.0, end: 60.0, top: 20.0, bottom: 40.0),
+            Padding(
+              padding: EdgeInsetsGeometry.directional(
+                start: 100.0,
+                end: 100.0,
+                top: 20.0,
+                bottom: 40.0,
+              ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                TextField(
-                style: const TextStyle(
-                  color: Colors.black87,
-                  fontSize: 16.0,
-                ),
-                decoration: InputDecoration(
-                  hintText: "Type the answer",
-                  hintStyle: TextStyle(
-                    color: Colors.grey[600],
-                    fontSize: 16.0,
-                  ),
-                  filled: true,
-                  fillColor: const Color(0xFFD9D9D9),
-                  contentPadding: const EdgeInsets.symmetric(
-                    horizontal: 24.0,
-                    vertical: 16.0,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16.0), 
-                    borderSide: BorderSide.none, 
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16.0),
-                    borderSide: BorderSide.none,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16.0),
-                    borderSide: const BorderSide(
-                      color: Color(0xFF7ED4DE), // Błękitny kolor (np. Light Blue)
-                      width: 3.0,               // Grubość ramki (żeby była wyraźna)
+                children: [
+                  TextField(
+                    style: const TextStyle(
+                      color: Colors.black87,
+                      fontSize: 25.0,
+                    ),
+                    decoration: InputDecoration(
+                      hintText: placeHolder,
+                      hintStyle: TextStyle(
+                        color: Colors.grey[600],
+                        fontSize: 25.0,
+                      ),
+                      filled: true,
+                      fillColor: const Color(0xFFD9D9D9),
+                      contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 24.0,
+                        vertical: 26.0,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16.0),
+                        borderSide: BorderSide.none,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16.0),
+                        borderSide: BorderSide.none,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16.0),
+                        borderSide: const BorderSide(
+                          color: Color(
+                            0xFF7ED4DE,
+                          ), // Błękitny kolor (np. Light Blue)
+                          width: 3.0, // Grubość ramki (żeby była wyraźna)
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 10.0, right: 20.0),
-                child: Text(
-                  "Don’t Know?  ",
-                  style: TextStyle(
-                    fontSize: 25.0,
-                    color: Color(0x88000000),
-                    decoration: TextDecoration.underline,
-                    decorationColor: Color(0x88000000),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10.0, right: 20.0),
+                    child: GestureDetector(
+                      onTap: onSkip,
+
+                      child: Text(
+                        "Don’t Know?  ",
+                        style: TextStyle(
+                          fontSize: 25.0,
+                          color: Color(0x88000000),
+                          decoration: TextDecoration.underline,
+                          decorationColor: Color(0x88000000),
+                        ),
+                        textAlign: TextAlign.end,
+                      ),
+                    ),
                   ),
-                  textAlign: TextAlign.end,
-                  
-                )
-                )
-              ],
+                ],
+              ),
             ),
-            ),
-            
-          ]
-        )
-      )
+          ],
+        ),
+      ),
     );
   }
 }
