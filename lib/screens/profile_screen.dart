859168b26c3ff4bfd666f2bd1eb97d298f6df014
Piotr Kierznowski <s197652@student.pg.edu.pl>
2026-01-05 16:00:44 +0100

    added profile header to the profile screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -12,7 +12,70 @@ class ProfileScreen extends StatelessWidget {
         backgroundColor: Color(0xFFE5E5E5),
         scrolledUnderElevation: 0.0,
       ),
-      body: Center(child: Text('Profile page')),
+      body: Column(children: [ProfileHeader()]),
+    );
+  }
+}
+
+class ProfileHeader extends StatelessWidget {
+  const ProfileHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 25.0, bottom: 25.0, left: 10.0, right: 10.0),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            color: Color(0x33000000),
+            width: 2,
+          ),
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Imie Nazwisko',
+                style: TextStyle(fontSize: 30.0, color: Colors.black, height: 1.0),
+              ),
+              Text(
+                'pobrany nick',
+                style: TextStyle(fontSize: 20.0, color: Color(0x88000000)),
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.access_time_filled,
+                    size: 16.0,
+                    color: Color(0x88000000), 
+                  ),
+                  SizedBox(width: 4.0),
+                  Text('pobrane data', style: TextStyle(fontSize: 15.0, height: 2.5)),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(width: 30.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                width: 90, 
+                height: 90,
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                  shape: BoxShape.circle,
+                ),
+                child: Center(child: Text('picture')),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
